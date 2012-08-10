d493474a47f76d2732b6fce5089e240773a39845
Martin Kleppe <kleppe@ubilabs.net>
2012-08-10 16:06:41 +0200

    Update build tool to keep copies for gh-pages branch.

--- build.sh
+++ build.sh
@@ -6,3 +6,13 @@ uglifyjs jquery.geocomplete.js > jquery.geocomplete.min.js
 ## create documentation
 docco jquery.geocomplete.js
 mv docs/jquery.geocomplete.html docs/index.html
+
+rm -Rf _temp
+
+mkdir _temp
+mkdir _temp/examples
+
+mv docs _temp
+cp jquery.geocomplete.js _temp/
+cp jquery.geocomplete.min.js _temp/
+cp examples/* _temp/examples
